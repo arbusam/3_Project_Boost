80dfb3a17beaec19db1cdffc082eb878c8512fdf
Arhan Busam <arhan.busam@gmail.com>
2021-02-18 19:07:50 +1100

    Updated Level Design

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
     Rigidbody rigidbody;
     AudioSource audioSource;
 
@@ -23,17 +26,34 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Ok");
+                break;
+            case "Fuel":
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
     private void Rotate()
     {
         rigidbody.freezeRotation = true;
 
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D)) // Can not rotate Right and Left at the same time
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidbody.freezeRotation = false;
@@ -41,9 +61,10 @@ public class Rocket : MonoBehaviour
 
     private void Thrust()
     {
+
         if (Input.GetKey(KeyCode.Space)) // Can thrust while simultanisly rotating
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
